82935397c76f65981b59177222be8db17020bfc7
Hasan Atmaca <104566717+Argeolog@users.noreply.github.com>
2023-04-03 11:30:20 +0300

    Add files via upload
    MyKey Dongle Universal

---
+++ C#/MyKey SDK (CSharp)/MyKey_DLL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyKey_SDK_CSharp
+{
+    
+
+
+    internal class MyKey_DLL
+    {
+
+        const string LibName = "System.My.Data.dll";
+             
+        [DllImport(LibName)]
+        public static extern IntPtr Login(string KeyData, string Pin);
+
+        [DllImport(LibName)]
+        public static extern IntPtr Serial_No_Read();
+
+        [DllImport(LibName)]
+        public static extern IntPtr Device_Model_Read();
+
+        [DllImport(LibName)]
+        public static extern IntPtr Device_Firmware_Read();
+
+        [DllImport(LibName)]
+        public static extern IntPtr Pin_Write(string Parametre, string YeniPin);
+        [DllImport(LibName)]
+        public static extern IntPtr Read_Protected_Data(string SessionID, string Pin, int Adres, string Key);
+        [DllImport(LibName)]
+        public static extern IntPtr Write_Protected_Data(string SessionID, string Pin, int Adres, string Key, string Data, string WRMode);
+
+        [DllImport(LibName)]
+        public static extern IntPtr Parametreleri_Yukle(string CihazParametre, string DefaultPin, string PinParametre);
+       
+
+
+    }
+}
---
+++ VB.Net/MyKey SDK/Anasayfa.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+
+Public Class Anasayfa
+
+
+    Private Sub Login_Buton_Click(sender As Object, e As EventArgs) Handles Login_Buton.Click
+        Try
+            Gelen_Session_Text.Text = Marshal.PtrToStringAnsi(Login(Parametre_Text.Text, Pin_Text.Text))
+            Giden_Session_Text.Text = Gelen_Session_Text.Text
+            Veri_Yazma_SessionID_Text.Text = Gelen_Session_Text.Text
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Data_Oku_Buton_Click(sender As Object, e As EventArgs) Handles Data_Oku_Buton.Click
+
+        Okunan_Eeprom_Data_Text.Text = Marshal.PtrToStringAnsi(Read_Protected_Data(Giden_Session_Text.Text, Data_Oku_Pin_Text.Text, CInt(Adres_Text.Text), Data_Key_Text.Text))
+
+    End Sub
+
+
+
+    Private Sub Cihaz_Model_Oku_Buton_Click(sender As Object, e As EventArgs) Handles Cihaz_Model_Oku_Buton.Click
+        Cihaz_Model_Text.Text = Marshal.PtrToStringAnsi(Device_Model_Read())
+    End Sub
+
+    Private Sub Serial_Oku_Buton_Click(sender As Object, e As EventArgs) Handles Serial_Oku_Buton.Click
+        Cihaz_SeriNo_Text.Text = Marshal.PtrToStringAnsi(Serial_No_Read())
+    End Sub
+
+    Private Sub Pin_Yukle_Buton_Click(sender As Object, e As EventArgs) Handles Pin_Yukle_Buton.Click
+
+        Dim Gelen As String = Marshal.PtrToStringAnsi(Pin_Write(Pin_Yukleme_Parametre.Text, Yuklenecek_Pin_Text.Text))
+        MsgBox(Gelen)
+    End Sub
+
+    Private Sub Data_Yaz_Buton_Click(sender As Object, e As EventArgs) Handles Data_Yaz_Buton.Click
+        Dim Gelen As String = Marshal.PtrToStringAnsi(Write_Protected_Data(Veri_Yazma_SessionID_Text.Text, Veri_Yazma_Pin_Text.Text, CInt(Veri_Yazma_Adres_Text.Text), Veri_Yazma_Data_Key_Text.Text, Veri_Yazma_Eeprom_Data_Text.Text, "RW"))
+        islem_Sonuc_Text.Text = Gelen
+    End Sub
+
+    Private Sub Veri_Yazma_Adres_Text_TextChanged(sender As Object, e As EventArgs) Handles Veri_Yazma_Adres_Text.TextChanged
+        Adres_Text.Text = Veri_Yazma_Adres_Text.Text
+    End Sub
+
+    Private Sub Login_Paramete_Yukle_Buton_Click(sender As Object, e As EventArgs) Handles Login_Paramete_Yukle_Buton.Click
+
+
+        Dim Hatavar As Boolean
+        If (Yuklenecek_Login_Parametre_Text.Text.Length <> 24) Then
+            MessageBox.Show("Yüklenecek Login Parametre 24 Karakterli Olmalıdır.")
+            Hatavar = True
+        ElseIf (Yuklenecek_Pin_Text.Text.Length <> 4) Then
+            MessageBox.Show("Yüklenecek Pin 4 Karakterli Olmalıdır.")
+            Hatavar = True
+
+        ElseIf (Yuklenecek_Pin_Parametre_Text.Text.Length <> 24) Then
+            MessageBox.Show("Yüklenecek Pin Parametresi 24 Karakterli Olmalıdır.")
+            Hatavar = True
+        End If
+
+        If Hatavar = False Then
+            Dim Cevap As String = Marshal.PtrToStringAnsi(Parametreleri_Yukle(Yuklenecek_Login_Parametre_Text.Text, Default_Pin_Text.Text, Yuklenecek_Pin_Parametre_Text.Text))
+            MsgBox(Cevap)
+        End If
+
+    End Sub
+    Private Sub Firmware_Oku_Buton_Click(sender As Object, e As EventArgs) Handles Firmware_Oku_Buton.Click
+
+        Dim Gelen As String = Marshal.PtrToStringAnsi(Device_Firmware_Read())
+        MsgBox(Gelen)
+    End Sub
+
+    Private Sub Anasayfa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
