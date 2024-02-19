79029951fb4b5f9b43dc5d36c6ed4763ba058c62
Hasan Atmaca <104566717+Argeolog@users.noreply.github.com>
2024-02-19 13:18:23 +0300

    Add files via upload

--- VB.Net/MyKey SDK/Anasayfa.vb
+++ VB.Net/MyKey SDK/Anasayfa.vb
@@ -34,6 +34,7 @@ Public Class Anasayfa
     Private Sub Pin_Yukle_Buton_Click(sender As Object, e As EventArgs) Handles Pin_Yukle_Buton.Click
 
         Dim Gelen As String = Marshal.PtrToStringAnsi(Pin_Write(Pin_Yukleme_Parametre.Text, Yuklenecek_Pin_Text.Text))
+        GC.Collect()
         MsgBox(Gelen)
     End Sub
 
@@ -41,6 +42,7 @@ Public Class Anasayfa
 
         Dim Gelen As String = Marshal.PtrToStringAnsi(Write_Protected_Data(Veri_Yazma_SessionID_Text.Text, CInt(Veri_Yazma_Adres_Text.Text), Veri_Yazma_Eeprom_Data_Text.Text, Veri_Yazma_Pin_Text.Text, Veri_Yazma_Data_Key_Text.Text, "RW"))
         islem_Sonuc_Text.Text = Gelen
+        GC.Collect()
     End Sub
 
     Private Sub Veri_Yazma_Adres_Text_TextChanged(sender As Object, e As EventArgs) Handles Veri_Yazma_Adres_Text.TextChanged
@@ -54,7 +56,7 @@ Public Class Anasayfa
         If (Yuklenecek_Login_Parametre_Text.Text.Length <> 24) Then
             MessageBox.Show("Yüklenecek Login Parametre 24 Karakterli Olmalıdır.")
             Hatavar = True
-        ElseIf (Yuklenecek_Pin_Text.Text.Length <> 4) Then
+        ElseIf (Default_Pin_Text.Text.Length <> 4) Then
             MessageBox.Show("Yüklenecek Pin 4 Karakterli Olmalıdır.")
             Hatavar = True
 
@@ -65,6 +67,7 @@ Public Class Anasayfa
 
         If Hatavar = False Then
             Dim Cevap As String = Marshal.PtrToStringAnsi(Parametreleri_Yukle(Yuklenecek_Login_Parametre_Text.Text, Default_Pin_Text.Text, Yuklenecek_Pin_Parametre_Text.Text))
+            GC.Collect()
             MsgBox(Cevap)
         End If
 
@@ -72,6 +75,7 @@ Public Class Anasayfa
     Private Sub Firmware_Oku_Buton_Click(sender As Object, e As EventArgs) Handles Firmware_Oku_Buton.Click
 
         Dim Gelen As String = Marshal.PtrToStringAnsi(Device_Firmware_Read())
+        GC.Collect()
         MsgBox(Gelen)
     End Sub
 
